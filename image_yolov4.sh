8895577b92ddbbac2a375350eb743307d5164738
solahn <solahn00@gmail.com>
2024-05-29 19:28:36 +0900

    add data

--- image_yolov4.sh
+++ image_yolov4.sh
@@ -2,5 +2,6 @@
 
 ./darknet detector test ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights data/test_car.jpg -i 0 -thresh 0.25
 
+#./darknet detector test ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights data/image_part_1.jpg -i 0 -thresh 0.25
 
 

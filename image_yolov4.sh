ce666ebd28ca69d7559bd115e90dde4959dfdbe6
solahn <solahn00@gmail.com>
2024-05-29 19:46:54 +0900

    minor fix

--- image_yolov4.sh
+++ image_yolov4.sh
@@ -1,8 +1,8 @@
 
 
-./darknet detector test ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights data/test_car.jpg -i 0 -thresh 0.25
+# ./darknet detector test ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights data/test_car.jpg -i 0 -thresh 0.25
 
-#./darknet detector test ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights data/image_part_4.jpg -i 0 -thresh 0.25
+./darknet detector test ./cfg/coco.data ./cfg/yolov4.cfg ./yolov4.weights data/image_part_9.jpg -i 0 -thresh 0.25
 
 # for file_num in {1..9}
 # do
